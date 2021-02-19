d2ec193db92e80313510259397662395df3e84c4
Pythagoras <Pythagoras@users.noreply.github.com>
2021-02-19 09:16:46 +0100

    Update MainController.java

--- src/control/MainController.java
+++ src/control/MainController.java
@@ -5,12 +5,12 @@ import model.Person;
 
 public class MainController {
 
-    private List<Person> allPersons;
-    private String[] names = {"Alsbach", "Bachmann", "Cyrus", "Davidoff", "Eregon", "Füller","Giesehau","Halidsch","Irimoff","Zylla","Yilderim","Lupp","Schein","Xenomo","Iwan","Ali","Kötter","Kleinhans","Diablo","Overwatch","Starcraft","Warcraft","Minecraft","Krunker","Command","And","Conquer","Path","Exile","Valheim"};
+    private final List<Person> allPersons;
+    private final String[] names = {"Alsbach", "Bachmann", "Cyrus", "Davidoff", "Eregon", "Füller", "Giesehau", "Halidsch", "Irimoff", "Zylla", "Yilderim", "Lupp", "Schein", "Xenomo", "Iwan", "Ali", "Kötter", "Kleinhans", "Diablo", "Overwatch", "Starcraft", "Warcraft", "Minecraft", "Krunker", "Command", "And", "Conquer", "Path", "Exile", "Valheim"};
 
     public MainController(int amount){
         allPersons = new List<>();
-        for(int i = 0; i < amount; i++){
+        for ( int i = 0; i < amount; i++ ) {
             allPersons.append(createPerson());
         }
     }
@@ -21,29 +21,31 @@ public class MainController {
     }
 
     public String getRandomName(){
-        return names[(int)(Math.random()*names.length)];
+        return names[ (int) ( Math.random() * names.length ) ];
     }
 
     public String showList(){
         String output = "Ausgabe: ";
         allPersons.toFirst();
-        while(allPersons.hasAccess()){
-            output = output + allPersons.getContent().getName()+" ("+allPersons.getContent().getBirthdate()+"), ";
+        while (allPersons.hasAccess()) {
+            output = output + allPersons.getContent().getName() + " (" + allPersons.getContent().getBirthdate() + "), ";
             allPersons.next();
         }
         return output;
     }
 
+
     /**
      * Schreibe einen Algorithmus zum Suchen einer Person innerhalb einer Liste. Falls die Person gefunden wurde, gib ihren Namen samt Geburtsdatum aus.
+     *
      * @param name
      * @return
      */
     public String searchList(String name){
         String output = "Nicht gefunden.";
         allPersons.toFirst();
-        while (allPersons.hasAccess()){
-            if(allPersons.getContent().getName().equalsIgnoreCase(name)){
+        while (allPersons.hasAccess()) {
+            if ( allPersons.getContent().getName().equalsIgnoreCase(name) ) {
                 output = "Name: " + allPersons.getContent().getName() + "  Geburtsdatum: " + allPersons.getContent().getBirthdate();
                 return output;
             }
@@ -53,14 +55,30 @@ public class MainController {
         return output;
     }
 
+
     /**
      * Stortiere die Liste nach Namen (nicht nach Geburtsdatum!). Entscheide dich hierzu für einen Sortieralgorithmus.
      * Gib an, ob deine Umsetzung stabil ist und ob sie in-place ist.
      */
     public void sortList(){
+        List<Person> help = new List<>();
+        help.concat(allPersons);
+        help.toFirst();
+        while (help.hasAccess()) {
+            allPersons.toFirst();
+            while (allPersons.hasAccess() && help.getContent().getName().compareTo(allPersons.getContent().getName()) >= 0) {
+                allPersons.next();
+            }
+            if ( allPersons.hasAccess() ) {
+                allPersons.insert(help.getContent());
+            } else {
+                allPersons.append(help.getContent());
+            }
+            help.next();
+        }
+
 
     }
-
 
 
 }
